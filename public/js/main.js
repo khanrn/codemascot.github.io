25b3bdda1740f73cdd3b23dcda43727fa2a0b130
Khan M Rashedun-Naby <naby88@gmail.com>
2017-10-25 13:20:17 +0600

    Main JS File Updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 (function( $ ){
 	'use strict';
 	$(function(){
-	
-	});
+		var current_year = new Date().getFullYear();
+        	$('span#age-year').text(current_year-1993);
+		$('span#experience-year').text(current_year-2012);
+        });
+
 })(jQuery);
